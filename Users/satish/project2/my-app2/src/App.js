3ff49cd0254369cd1037b60611539f61956a01f1
satish <satish3r3@gmail.com>
2021-06-26 08:07:07 -0400

    react LogginController

---
+++ Users/satish/project2/my-app2/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+
+import Clock from './clock.component'
+function App() {
+  return (
+    <div className="App">
+     <Clock/>
+    </div>
+  );
+}
+
+export default App;
